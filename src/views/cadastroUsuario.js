59c9dcbe0e6380f4cfd754948c98d935616a2864
flaviofonseca-smartnx <flavio.fonseca@smartnx.com>
2022-09-26 23:26:59 -0300

    alertas toastr

--- src/views/cadastroUsuario.js
+++ src/views/cadastroUsuario.js
@@ -21,7 +21,41 @@ class CadastrarUsuario extends React.Component{
         this.service = new UsuarioService()
     }
 
-    cadastrar = () =>{      
+    validar(){
+
+        const msgs = [];
+
+        if(!this.state.nome){
+            msgs.push('Campo nome é obrigatório.')
+        }
+
+        if(!this.state.email){
+            msgs.push('Campo E-mail é obrigatório.')
+        }else if(!this.state.email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/)) {
+            msgs.push('Informe um E-mail válido.')
+        }
+
+        if(!this.state.senha || !this.state.senhaRepeticao ){
+            msgs.push('Campo a senha 2x.')
+        }else if(this.state.senha !== this.state.senhaRepeticao ){
+            msgs.push('Senhas não conferem...')
+        }
+
+        return msgs;
+
+    }
+
+    cadastrar = () =>{     
+        const msgs = this.validar();
+
+        if(msgs && msgs.length > 0 ){
+
+            msgs.forEach( (msg, index) => {
+                mensagemErro(msg)
+            });
+
+            return false
+        } 
 
         const usuario = {
             nome:  this.state.nome,

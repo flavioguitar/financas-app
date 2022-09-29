cf4d5de050d1f31edff1af2d89a5151bec69b8d3
flaviofonseca-smartnx <flavio.fonseca@smartnx.com>
2022-09-28 23:32:20 -0300

    lancamentos

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -3,6 +3,7 @@ import  React from 'react';
 import Login from '../views/login';
 import CadastrarUsuario from '../views/cadastroUsuario';
 import Home from '../views/home';
+import consultaLancamentos from '../views/consulta-lancamentos';
 
 import { Route, Switch, HashRouter } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function Rotas(){
                 <Route path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastrarUsuario} />
+                <Route path="/consulta-lancamentos" component={consultaLancamentos} />
             </Switch> 
         </HashRouter>
     )

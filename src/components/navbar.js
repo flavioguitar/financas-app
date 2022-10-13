96e2e0029b3ab13e99bd147ee10ea435c3d95991
flaviofonseca-smartnx <flavio.fonseca@smartnx.com>
2022-10-13 17:24:58 -0300

    CAL-2500 PRIORIZACAO URA TELEVENDAS DPSP

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ function NavBar(){
 
     <div className="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
       <div className="container">
-        <a href="https://bootswatch.com/" className="navbar-brand">Minhas Finanças</a>
+        <a href="#/home" className="navbar-brand">Minhas Finanças</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
--- src/main/App.js
+++ src/main/App.js
@@ -5,13 +5,20 @@ import NavBar from "../components/navbar";
 import 'toastr/build/toastr.min.js'
 import 'bootswatch/dist/flatly/bootstrap.css'
 import '../custom.css'
-import 'toastr/build/toastr.css'
+import 'toastr/build/toastr.css'   
+
+import "primereact/resources/themes/lara-light-indigo/theme.css"
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";  
+
+import { Button } from 'primereact/button';
 class App extends React.Component {
 
   render(){
     return (
       <>
         <NavBar/>
+        <Button label="Click" icon="pi pi-check" />
         <div className="container">
           <Rotas/>
       </div>

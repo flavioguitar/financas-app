f3c606f809f2bc23d75e765024a7c8ee9d92b5b0
flaviofonseca-smartnx <flavio.fonseca@smartnx.com>
2022-10-17 23:05:12 -0300

    confirmação de dele

--- src/main/App.js
+++ src/main/App.js
@@ -10,15 +10,12 @@ import 'toastr/build/toastr.css'
 import "primereact/resources/themes/lara-light-indigo/theme.css"
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";  
-
-import { Button } from 'primereact/button';
 class App extends React.Component {
 
   render(){
     return (
       <>
-        <NavBar/>
-        <Button label="Click" icon="pi pi-check" />
+        <NavBar/>        
         <div className="container">
           <Rotas/>
       </div>
--- src/views/lancamentos/consulta-lancamentos.js
+++ src/views/lancamentos/consulta-lancamentos.js
@@ -8,6 +8,11 @@ import LancamentoService from "../../app/service/lancamentoService";
 import LocalStorageService from "../../app/service/localStorageService";
 
 import * as messages from '../../components/toastr';
+
+import {Dialog} from 'primereact/dialog';
+import {Button} from 'primereact/button';
+
+
 class ConsultaLancamentos extends React.Component{
 
     state = {
@@ -15,6 +20,8 @@ class ConsultaLancamentos extends React.Component{
         mes: '',
         descricao: '',
         tipo: '',
+        showConfirmDialog: false,
+        lancamentoDeletar: {},
         lancamentos: []
     }
 
@@ -48,15 +55,26 @@ class ConsultaLancamentos extends React.Component{
         })
     }
 
+    AbrirConfirmacao = (lancamento) => {
+
+        this.setState({ showConfirmDialog: true,lancamentoDeletar: lancamento })
+
+    }
+
+    CancelarDelecao = () => {
+        this.setState({ showConfirmDialog: false,lancamentoDeletar: {} })
+    }
+
     deletar = (lancamento) => {
-       this.service.deletar(lancamento.id)
+       this.service.deletar(this.state.lancamentoDeletar.id)
        .then(response =>{
             const lancamentos = this.state.lancamentos;
-            const index = lancamentos.indexOf(lancamento)
+            const index = lancamentos.indexOf(this.state.lancamentoDeletar)
             lancamentos.splice(index,1)
             this.setState(lancamentos)
 
             messages.mensagemSucesso("Lancamento deletado com sucesso")
+            this.setState({ showConfirmDialog: false,lancamentoDeletar: {} })
        }).catch(error =>{
             messages.mensagemErro("Erro ao deletar o lancamento")
        })
@@ -69,7 +87,15 @@ class ConsultaLancamentos extends React.Component{
     render(){
         const meses = this.service.obterListaMeses()
 
-        const tipos = this.service.obterListaTipos() 
+        const tipos = this.service.obterListaTipos()
+        
+        const confirmDialogFooter = (
+            <div>
+                <Button label="Confirmar" icon="pi pi-check" onClick={this.deletar} />
+                <Button label="Cancelar" icon="pi pi-times" onClick={this.CancelarDelecao} 
+                        className="p-button-secondary"/>
+            </div>
+        );
 
         return (
             <Card title="Consulta Lançamentos">
@@ -119,11 +145,21 @@ class ConsultaLancamentos extends React.Component{
                     <div className="col-md-12">
                         <div className="bs-component">
                             <LancamentosTable lancamentos={this.state.lancamentos} 
-                                              deleteAction={this.deletar}
+                                              deleteAction={this.AbrirConfirmacao}
                                               editAction={this.editar}
                             />
                         </div>
                     </div>
+                </div>
+                <div>
+                    <Dialog header="Confirmação"
+                            visible={this.state.showConfirmDialog}
+                            style={{width: '50vw'}}
+                            footer={confirmDialogFooter}
+                            modal={true}
+                            onHide={() => this.setState({showConfirmDialog: false}) }>
+                                Confirma a exclusão deste lançamento?
+                    </Dialog>
                 </div>
             </Card>
         )

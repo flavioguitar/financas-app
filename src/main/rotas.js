6330b5c574c1f21880eade342060f441aeb3a1d4
flaviofonseca-smartnx <flavio.fonseca@smartnx.com>
2022-11-03 16:45:12 -0300

    icones de botões

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -6,17 +6,36 @@ import Home from '../views/home';
 import consultaLancamentos from '../views/lancamentos/consulta-lancamentos';
 import cadastroLancamentos from '../views/lancamentos/cadastro-lancamento';
 
-import { Route, Switch, HashRouter } from 'react-router-dom';
+import { Route, Switch, HashRouter, Redirect } from 'react-router-dom';
 
+const isUsuarioAutenticado = () => {
+    return false;
+}
+
+function RotaAutenticada({ component: Component, ...props }){
+    return (
+        <Route {...props} render = { (componentProps) => {
+            if(isUsuarioAutenticado()){
+              return (
+                <Component {...componentProps} />
+              )  
+            }else{
+                return(
+                    <Redirect to={ {pathname: '/login', state: {from:componentProps.location}} } />
+                )
+            }
+        } } />
+    )
+}
 function Rotas(){
     return (
         <HashRouter>
             <Switch>
-                <Route path="/home" component={Home} />
+                <RotaAutenticada path="/home" component={Home} />
                 <Route path="/login" component={Login} />
                 <Route path="/cadastro-usuarios" component={CadastrarUsuario} />
-                <Route path="/consulta-lancamentos" component={consultaLancamentos} />
-                <Route path="/cadastro-lancamentos/:id?" component={cadastroLancamentos} />
+                <RotaAutenticada path="/consulta-lancamentos" component={consultaLancamentos} />
+                <RotaAutenticada path="/cadastro-lancamentos/:id?" component={cadastroLancamentos} />
             </Switch> 
         </HashRouter>
     )
--- src/views/home.js
+++ src/views/home.js
@@ -43,10 +43,10 @@ class Home extends React.Component{
             <p className="lead">
               <a className="btn btn-primary btn-lg"
                  href="#/cadastro-usuarios" 
-                 role="button"><i className="fa fa-users"></i>  Cadastrar Usuário</a>
+                 role="button"><i className="pi pi-users"></i>  Cadastrar Usuário</a>
               <a className="btn btn-danger btn-lg"
                  href="#/cadastro-lancamentos" 
-                 role="button"><i className="fa fa-users"></i>  Cadastrar Lançamento</a>
+                 role="button"><i className="pi pi-money-bill"></i>  Cadastrar Lançamento</a>
             </p>
           </div>
 
